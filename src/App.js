2087d7f138405a082a4d465ac2097d41205e297c
fahim <fahimahamedweb@gmail.com>
2023-09-22 02:41:07 +0600

    added some codes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import Companies from "./Components/Companies";
 import Hero from "./Components/Hero";
 import Guide from "./Components/Guide";
 import Properties from "./Components/Properties";
+import Details from "./Components/Details";
+import GetStarted from "./Components/GetStarted";
+import Footer from "./Components/Footer";
 function App() {
   return (
     <>
@@ -9,6 +12,9 @@ function App() {
       <Companies />
       <Guide />
       <Properties />
+      <Details />
+      <GetStarted />
+      <Footer />
     </>
   );
 }
